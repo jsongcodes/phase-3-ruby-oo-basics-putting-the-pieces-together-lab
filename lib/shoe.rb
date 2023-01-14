da597ccdf61eb55b4e53d7d27d2b6cfd9473364a
Jasmin <ssongnari@gmail.com>
2023-01-14 12:44:37 -0500

    commit tree 4323c487d03b2c58b62cc6271d88edc389a09d25

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,6 @@
 class Shoe
-    attr_accessor :color, :size, :material, :condition, :cobble
-    attr_reader :brand
-
+    attr_accessor :brand, :color, :size, :material, :condition
+    
     def initialize(brand)
         @brand = brand
     end
